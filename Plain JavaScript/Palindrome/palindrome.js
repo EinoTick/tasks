f3f116e6949e4f9fe23b43911fbe12105e03e107
Eino Tick <einotick@yahoo.com>
2019-08-28 11:05:13 +0300

    Add JS tool for checking palindromes

---
+++ Plain JavaScript/Palindrome/palindrome.js
@@ -0,0 +1,20 @@
+function palindromeForm() {
+  // regex [A-Za-z0-9 _.,!"'/$]*
+  var regex = new RegExp("[^A-Za-z0-9 :;.,!]");
+  var palWord = document.getElementById("formGroupExampleInput").value;
+  !!palWord ? 
+        (!palWord.match(regex) ? palindromeCheck(palWord) : alert("Please use only letters(a-z), numbers(0-9), space and punctuation!")) 
+        : alert("Please, fill the form!");
+        
+  alert(palWord.test(regex));      
+}
+
+function palindromeCheck(word) {
+  reverseWord = word.split('').reverse().join('');
+      
+  if (reverseWord === word) {
+    document.getElementById("paliResult").innerHTML   = "" + word + " is palindrome";
+  } else {
+    document.getElementById("paliResult").innerHTML   = "" + word + " is not palindrome"; 
+  }
+}
